b8e024f29e380b315950e05daf44355fee1c70b1
umaima222 <105052639+umaima222@users.noreply.github.com>
2022-05-06 17:30:47 +0500

    changes

--- inc/header.php
+++ inc/header.php
@@ -1,133 +1,76 @@
 <style>
 </style>
 <header>
-    <div class="container">
+<div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="nav-container">
 
                 <div class="logo-img">
-                    <img src="img/logo.png" alt="" class="img-fluid">
+                    <img src="logo.png" alt="" class="img-fluid">
                 </div>
                 <div class="about-nav">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item dropdown">
-                            <a href="mortgage.php" class="btn btn-secondary dropdown-toggle"  role="button" id="navbarDropdown1" data-toggle="drop-down" aria-haspopup="true" aria-expanded="false">
-    Dropdown link
-  </a>
-                                <!-- <a href="mortgage.php" class="nav-link" id="navbarDropdown1" role="button"  data-toggle="dropdown" >
-                                    Mortgage
-                                    <i class="fa fa-angle-down"></i>
-                                </a> -->
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                                    <a class="dropdown-item" href="#">Refinance mortgage lenders</a>
-                                    <a class="dropdown-item" href="#">Compare current mortgage rates</a>
-                                    <a class="dropdown-item" class="nav-link" href="#" id="navbarDropdown12" role="button" data-toggle="dropdown">
-                                        Credit Cards
-                                        <i class="fa fa-angle-down"></i></a>
-                                    <div class="dropdown-menu side" aria-labelledby="navbarDropdown12">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-
-                                
-                            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle"href="mortgage.php" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link 
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-
-                            <li class="nav-item dropdown">
-                                <a class="nav-link" href="mortgage.php" id="navbarDropdown10" role="button"  data-toggle="dropdown" >
-                                    Mortgage
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown10">
-                                    <a class="dropdown-item" href="#">Refinance mortgage lenders</a>
-                                    <a class="dropdown-item" href="#">Compare current mortgage rates</a>
-                                    <a class="dropdown-item" class="nav-link" href="#" id="navbarDropdown102" role="button" data-toggle="dropdown">
-                                        Credit Cards
-                                        <i class="fa fa-angle-down"></i></a>
-                                    <div class="dropdown-menu side" aria-labelledby="navbarDropdown102">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Another action</a>
-                                        <a class="dropdown-item" href="#">Something else here</a>
-                                    </div>
-
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                            <!-- <li class="dropdown">
-                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown button
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <ul class="nav" >
+                        <li class="nav-item">
+                            <a class="nav-link1 " >Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link1 " href="file:///C:/xampp/htdocs/lendmemoney/dropdown.html">
+                                Mortgage <i class="fa fa-angle-down"></i></a>
+                            <div class="dropdown-menu1">
+                                <a class="dropdown-item" href="#">Refinance mortgage lenders</a>
+                                <a class="dropdown-item" href="#">Compare current mortgage rates</a>
+                                <a class="dropdown-item" class="nav-link" href="#" id="navbarDropdown12" role="button" data-toggle="dropdown">
+                                    Credit Cards
+                                    <i class="fa fa-angle-down"></i></a>
+                                <div class="dropdown-menu side" aria-labelledby="navbarDropdown12">
                                     <a class="dropdown-item" href="#">Action</a>
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <a class="dropdown-item" href="#">Something else here</a>
                                 </div>
-                                </li> -->
-                            <li class="nav-item dropdown">
-                                <a class="nav-link" href="mortgage-advisor.php" id="navbarDropdown3" role="button" data-toggle="dropdown">
-                                    Insurance
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
-                                    <a class="dropdown-item" href="#">Auto insurance</a>
-                                    <a class="dropdown-item" href="#">life insurance</a>
-                                    <a class="dropdown-item" href="#">home insurance</a>
-                                    <a class="dropdown-item" href="#">about life insurance calculator </a>
-                                    <a class="dropdown-item" href="#">pet insurance</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link" href="#" id="navbarDropdown4" role="button" data-toggle="dropdown">
-                                    Credit Cards
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link" href="#" id="navbarDropdown5" role="button" data-toggle="dropdown">
-                                    Loan
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown5">
-                                    <a class="dropdown-item" href="#">personal loan</a>
-                                    <a class="dropdown-item" href="#">home equity loan</a>
-                                    <a class="dropdown-item" href="#">Auto Refinance</a>
-                                    <a class="dropdown-item" href="#">Shudent loan Refinance</a>
-                                </div>
-                            </li>
-                            <li class="nav-item nbar">
-                                <a class="nav-link navlink">Become A Member</a>
-                            </li>
-                            <li class=" nav-item  nbar">
-                                <a class="nav-link navlink">Login/Register <i class="fa fa-search" style="color: #88B379;"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link1 ">
+                                Insurance <i class="fa fa-angle-down"></i></a>
+                            <div class="dropdown-menu1">
+                                <a class="dropdown-item" href="#">Auto insurance</a>
+                                <a class="dropdown-item" href="#">life insurance</a>
+                                <a class="dropdown-item" href="#">home insurance</a>
+                                <a class="dropdown-item" href="#">about life insurance calculator </a>
+                                <a class="dropdown-item" href="#">pet insurance</a>
+
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link1" href="#"> Credit Cards
+                                <i class="fa fa-angle-down"></i></a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link1 " id="drop-down-tab" data-toggle="dropdown" href="#" role="tab"
+                                aria-controls="profile" aria-selected="true">
+                                Loan <i class="fa fa-angle-down"></i></a>
+                            <div class="dropdown-menu1">
+                                <a class="dropdown-item" href="#">personal loan</a>
+                                <a class="dropdown-item" href="#">home equity loan</a>
+                                <a class="dropdown-item" href="#">Auto Refinance</a>
+                                <a class="dropdown-item" href="#">Shudent loan Refinance</a>
+
+                            </div>
+                        </li>
+                        <li class="nav-item nbar">
+                            <a class="nav-link disabled">Become A Member</a>
+                        </li>
+                        <li class="nav-item nbar">
+                            <a class="nav-link disabled">Login/Register <i class="fa fa-search"
+                                    style="color: #88B379;"></i>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
-            <div class="canvas-icon text-right">
-                <i class="fa fa-bars" style="color: black;" aria-hidden="true"></i>
-            </div>
 
+            </div>
         </nav>
-
     </div>
 </header>
 <div class="mobile-header">
@@ -167,7 +110,7 @@
         </li>
         <li class="nav-item"><a href=""> Credit Cards <i class="fa fa-angle-down"></i>
         <li class="nav-item dropdown">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="">
                 Loan
                 <i class="fa fa-angle-down"></i>
             </a>
